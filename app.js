93aa4cc9c4540d10300f71b907b7b90319b74440
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-02-03 14:33:18 +0300

    The ejs module was used and the relevant directions were made

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const { send } = require("express/lib/response");
+const ejs = require("ejs");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+//TEMPLATES ENGINE
+app.set("view engine", "ejs");
 
-  res.send(blog);
+//MIDDLEWARE
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
